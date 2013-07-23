0eaaca7300added3d4f3151e633ffda970e2ce98
Andres Arellano <andres.arellano@gmail.com>
2013-07-23 17:17:16 -0400

    Adds the websocket-rails event router

---
+++ config/initializers/events.rb
@@ -0,0 +1,6 @@
+WebsocketRails::EventMap.describe do
+  # The :client_connected method is fired automatically when a new client connects
+  subscribe :client_connected, to: ChatController, with_method: :client_connected
+
+  subscribe :new_message, to: ChatController, with_method: :new_message
+end
