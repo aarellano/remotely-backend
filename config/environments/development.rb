740b0e489e5bfe8c660302c819a8789d088cc518
Andres Arellano <andres.arellano@gmail.com>
2013-07-23 16:08:25 -0400

    dev: delete Rack::Lock

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,5 @@ RemotelyBackend::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+  config.middleware.delete Rack::Lock
 end
