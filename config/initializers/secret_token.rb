fac3201f128b7cd4b07a85a7d415dfe8d5d5cd86
Andres Arellano <andres.arellano@gmail.com>
2013-07-23 16:00:09 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RemotelyBackend::Application.config.secret_key_base = 'ef7cf22d7cad58d17b84d77543301cab6e1ffb76b93492ec0f81a87ce7584d9873a765b89acdc288b6e5db9079d0d66da647383a350c1181e66aac56f4610feb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RemotelyBackend::Application.config.session_store :cookie_store, key: '_remotely-backend_session'
